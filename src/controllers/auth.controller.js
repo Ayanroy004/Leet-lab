1cb0a20ca79ab1976f4a6b56d1d098aea5fad5d9
Ayan Roy <154493815+Ayanroy004@users.noreply.github.com>
2025-06-02 23:31:50 +0530

    Authentication add

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,6 @@
 import bcrypt from "bcryptjs";
 import { db } from "../libs/db.js";
 import { UserRole } from "../generated/prisma/index.js";
-
 import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
@@ -47,6 +46,7 @@ export const register = async (req, res) => {
     });
 
     res.status(201).json({
+      success: true,
       message: "User registered successfully",
       user: {
         id: newUser.id,
@@ -62,6 +62,96 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {};
-export const logout = async (req, res) => {};
-export const check = async (req, res) => {};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      res.status(401).json({
+        message: "User not found",
+      });
+    }
+    console.log("user:", user);
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
+      sameSite: "strict", // Prevent CSRF attacks
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User Login successfully",
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role,
+        image: user.image,
+      },
+    });
+  } catch (error) {
+    console.log("Error login user:", error);
+    res.status(500).json({
+      error: "error logging in user",
+    });
+  }
+};
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
+      sameSite: "strict", // Prevent CSRF attacks
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User logged out successfully",
+    });
+  } catch (error) {
+    console.log("Error logging out user:", error);
+    res.status(500).json({
+      error: "error logging out user",
+    });
+  }
+};
+export const check = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "User is authenticated",
+      user: req.user,
+    });
+  } catch (error) {
+    console.log("Error checking user authentication:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,56 @@
+import jwt from "jsonwebtoken";
+import { db } from "../libs/db.js";
+
+export const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.cookies?.jwt;
+    console.log("Token received:");
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized, no token provided",
+      });
+    }
+    let decoded;
+    
+    try {
+      decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized, invalid token",
+      });
+    }
+
+    const user = await db.user.findUnique({
+      where:{
+        id : decoded.id
+      },
+      select:{
+        id: true,
+        email: true,
+        name: true,
+        role: true,
+        image: true
+      }
+    })
+
+    if(!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    req.user = user;
+    next();
+
+  } catch (error) {
+    console.log("Error in auth middleware:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
