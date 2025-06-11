48f24da428265d2014e581e3eb2e3b66450b4453
Ayan Roy <154493815+Ayanroy004@users.noreply.github.com>
2025-06-11 23:16:34 +0530

    cors add

--- src/index.js
+++ src/index.js
@@ -1,28 +1,37 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import authRoutes from './routes/auth.routes.js';
-import problemRoutes from './routes/problem.routes.js';
-import executionRoute from './routes/executeCode.routes.js';
-import submissionRoutes from './routes/submission.routes.js';
-import playlistRoutes from './routes/playlist.routes.js';
-dotenv.config('./.env');
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.routes.js";
+import problemRoutes from "./routes/problem.routes.js";
+import executionRoute from "./routes/executeCode.routes.js";
+import submissionRoutes from "./routes/submission.routes.js";
+import playlistRoutes from "./routes/playlist.routes.js";
+dotenv.config("./.env");
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "SET"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
 
+app.get("/", (req, res) => {
+  res.send("Hello all done");
+});
 
-app.get('/', (req, res)=>{
-  res.send("Hello all done")
-})
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/problems", problemRoutes);
+app.use("/api/v1/execute-code", executionRoute);
+app.use("/api/v1/submission", submissionRoutes);
+app.use("/api/v1/playlist", playlistRoutes);
 
-app.use("/api/v1/auth", authRoutes)
-app.use("/api/v1/problems", problemRoutes)
-app.use("/api/v1/execute-code", executionRoute)
-app.use("/api/v1/submission", submissionRoutes)
-app.use("/api/v1/playlist", playlistRoutes)
-
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
-})
+});
